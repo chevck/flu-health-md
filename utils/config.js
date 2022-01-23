d39e7fe25b0ed4a47becb9f54ed7974a5fa0f9de
Excellence <oyeniranexcellenced@gmail.com>
2022-01-23 23:50:10 +0100

    changed prod mongo url

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   production: {
     mongoUrl:
-      "mongodb+srv://excellence:excelly2000@cluster-health.vq819.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      "mongodb+srv://excellence:excelly2000@cluster0.w0j2i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   },
   dev: {
     mongoUrl:
